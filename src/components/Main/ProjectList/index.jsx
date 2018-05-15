0ef83e8e2538050b37cd460420895020eb8c483f
Jie Liang <lj925184928@gmail.com>
2018-05-15 11:32:03 -0500

    Fix #156: Use UTC in project page

--- src/components/Main/ProjectList/index.jsx
+++ src/components/Main/ProjectList/index.jsx
@@ -172,7 +172,7 @@ class ProjectList extends Component {
               </Modal>
             </div>
             <div className='rt-td' style={{flex: '200 0 auto', width: '200px'}}><span className=''>{project.stage}</span></div>
-            <div className='rt-td Number' style={{flex: '150 0 auto', width: '150px'}}>{moment.unix(project.applicationExpiry).format('YYYY-MM-DD HH:mm:ss')}</div>
+            <div className='rt-td Number' style={{flex: '150 0 auto', width: '150px'}}>{moment.unix(project.applicationExpiry).utc().format('YYYY-MM-DD HH:mm:ss')}</div>
             <div className='rt-td' style={{flex: '200 0 auto', width: '200px'}}>
               {!this.actionNeedModal(project) &&
                 <a onClick={(e) => { this.updateStatus(e, project) }} className='ui mini button purple' href='#!'>{project.action}</a>
